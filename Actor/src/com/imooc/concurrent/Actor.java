b552c64dbc864ac469437962658e7b7bdc224451
quietcoolwu <quietcool@163.com>
2015-05-28 02:24:14 +0800

    Javaselfteach_Intelij
    Some code just for excercise and fun!
    
    Test1 on May 28th.
    
    Test2 at 1:40AM.
    
    Test3 at 2:10AM.

--- Actor/src/com/imooc/concurrent/Actor.java
+++ Actor/src/com/imooc/concurrent/Actor.java
@@ -36,7 +36,54 @@ public class Actor extends Thread{
         actor.setName("Mr.Thread");
 
         actor.start();
+
+        Thread actressThread = new Thread(new Actress(), "Ms.Runnable");
+        actressThread.start();
     }
 
 
+}
+
+
+class Actress implements Runnable{
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+        System.out.println(Thread.currentThread().getName()+"是一个演员!");
+        int count = 0;
+        boolean keepRunning = true;
+        while(keepRunning){
+            System.out.println(Thread.currentThread().getName()+"登台演出:"+(++count));
+            if(count == 100){
+                keepRunning =false;
+            }
+
+
+            if(count%10 == 0){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+
+
+        System.out.println(Thread.currentThread().getName()+"的演出结束了!");
+
+    }
 }
