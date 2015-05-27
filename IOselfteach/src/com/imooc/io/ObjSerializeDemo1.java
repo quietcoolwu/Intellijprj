acc8c9e30e78072fc1e7f194a042d5400cc89ed5
quietcoolwu <quietcool@163.com>
2015-05-28 00:45:59 +0800

    Javaselfteach_Intelij
    Some code just for excercise and fun!
    
    Test1 on May 28th.

--- IOselfteach/src/com/imooc/io/ObjSerializeDemo1.java
+++ IOselfteach/src/com/imooc/io/ObjSerializeDemo1.java
@@ -11,17 +11,19 @@ import java.io.ObjectOutputStream;
 public class ObjSerializeDemo1 {
 
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String file = "demo/obj.dat";
         //对象的序列化
-        ObjectOutputStream oos =  new ObjectOutputStream(new FileOutputStream(file));
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
         Student stu = new Student("10001", "张三", 20);
         oos.writeObject(stu);
         oos.flush();
         oos.close();
 
-        ObjectInputStream ois =new ObjectInputStream(new FileInputStream(file));
-        Student stu_out = (Student)ois.readObject();
+
+        //对象的反序列化
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        Student stu_out = (Student) ois.readObject();
         System.out.println(stu_out);
         ois.close();
     }
--- IOselfteach/src/com/imooc/io/Student.java
+++ IOselfteach/src/com/imooc/io/Student.java
@@ -1,7 +1,6 @@
 package com.imooc.io;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
 /**
  * Created by William on 2015/5/26.
@@ -57,7 +56,13 @@ public class Student implements Serializable {
             throws java.io.IOException {
         s.defaultWriteObject();//把jvm默认能进行序列化的元素序列化
         s.writeInt(stuAge);//自己完成stuAge的序列化
+        //ArrayList
+    }
 
+    private void readObject(java.io.ObjectInputStream s)
+            throws java.io.IOException, ClassNotFoundException {
+        s.defaultReadObject();//把jvm默认能反序列话的元素进行反序列化
+        this.stuAge = s.readInt();//自己完成反序列化操作
     }
 
 
